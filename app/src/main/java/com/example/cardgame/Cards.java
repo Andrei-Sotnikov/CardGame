f04f30fabc173b5a7c224e95096dce21ff847cac
Andrei-Sotnikov <mirrox.sotnikoff@gmail.com>
2020-02-25 18:20:36 +0300

    Сделал подобие масштабирования

--- app/src/main/java/com/example/cardgame/Cards.java
+++ app/src/main/java/com/example/cardgame/Cards.java
@@ -1,4 +1,15 @@
 package com.example.cardgame;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
 public class Cards {
+    private Resources resources;
+    Bitmap card;
+
+    public Cards(Resources resources , Bitmap card){
+        this.card = card;
+        this.resources = resources;
+    }
 }
